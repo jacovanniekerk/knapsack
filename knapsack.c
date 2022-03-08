06969425c051273b80ec744345d1331b2b006c79
Jaco van Niekerk <sparky@imaginet.co.za>
2022-03-08 07:19:27 +0200

    Added data, brute force working

--- knapsack.c
+++ knapsack.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct item
 {
     char description[25];
     int value;
-    int weight;
+    int cost;
 } item_t;
 
 /* A very flaky routine to read in the data.  Assumes that the data
@@ -14,54 +15,93 @@ typedef struct item
 int get_items(char *filename, item_t **data)
 {    
     FILE* file = fopen(filename, "r");
-    char line[256], *token = NULL;    
-    int i, cnt; 
+    char line[256], *token = NULL;
+    int i, cnt;
 
     /* Get numbner of items, allocate memory? */
-    fgets(line, sizeof(line), file);    
-    cnt = (int)strtol(token = strtok(line, ","), (char **)NULL, 10);        
-    *data = (item_t *)malloc(sizeof(item_t) * cnt);
+    if (!fgets(line, sizeof(line), file)) return 0;
+    cnt = (int)strtol(token = strtok(line, ","), (char **)NULL, 10);
+    *data = (item_t *)malloc(cnt * sizeof(item_t));
 
     /* Read in the items. */
     for (i = 0; i < cnt; i++)
-    {
-
-    } 
-
-    /*while (fgets(line, sizeof(line), file)) {       
-        token = strtok(line, ",");
-        printf("Item : [%s]\n", token);
-
-        token = strtok(NULL, ",");
-        if (token == NULL) continue;
-
-
-        tmp =  (int)strtol(token, (char **)NULL, 10);
-        printf("Value: [%d]\n", tmp);
-        
-        token = strtok(NULL, ",");
-        tmp =  (int)strtol(token, (char **)NULL, 10);
-        printf("Cost : [%d]\n", tmp);
-
-    }*/
-
-    /* may check feof here to make a difference between eof and io failure -- network
-       timeout for instance */
-
+    {        
+        if (!fgets(line, sizeof(line), file)) break;            
+        strcpy((*data)[i].description, token = strtok(line, ","));        
+        (*data)[i].value = (int)strtol(token = strtok(NULL, ","), (char **)NULL, 10);                
+        (*data)[i].cost = (int)strtol(token = strtok(NULL, ","), (char **)NULL, 10);
+    }     
     fclose(file);
     return cnt;
 }
 
+void quickprint(item_t *items, int n, unsigned long combo)
+{
+    int i, curv=0, cost=0;
+    for (i = 0; i < n; i++)
+    {
+        if ((combo >> i) & 1) {
+            printf("%s  value=%d, cost=%d\n", 
+                items[i].description, items[i].value, items[i].cost);
+            curv += items[i].value;
+            cost += items[i].cost;
+        }
+    }
+    printf("\nTotal value=%d, Total cost=%d\n", curv, cost);
+}
+
+/* Assumes 2^n <= max(unsigned long) */
+unsigned long bruteforce(item_t *items, int n, int max)
+{
+    unsigned long i, combo;
+    int j, curv, cost, best;
+
+    if ((sizeof(unsigned long)<<3) < max)
+    {
+        printf("Won't fit!");
+        return 0;
+    }
+
+
+    for (i = 0; i <= ((unsigned long)2) << n; i++)
+    {
+        curv = cost = 0;
+        for (j = 0; j < n; j++)
+        {
+            if ((i >> j) & 1) {
+                curv += items[j].value;
+                cost += items[j].cost;
+                if (cost > max) {
+                    break;
+                }
+            }
+        }
+        if (cost <= max && curv > best)
+        {
+            best = curv;
+            combo = i;
+        }
+    }
+    return combo;
+}
 
 int main(int argc, char *argv[])
 {
+    unsigned long combo;
     int cnt;
     item_t *d = NULL;
-    
+        
     cnt = get_items("data-small", &d);
+    
+    /*printf("found %d\n size=%lu\n", cnt, sizeof(unsigned long));
+    for (i = 0; i < cnt; i++)
+    {
+        printf("%s %d %d\n", d[i].description, d[i].value, d[i].cost);
+    }*/
+
+    combo = bruteforce(d, cnt, 5);
+    quickprint(d, cnt, combo);
+
     free(d);
-
-    printf("found %d\n", cnt);
-
     return 0;
 }
