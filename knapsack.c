a547b74d2044e6dc57257112ea0fb9989b46a174
Jaco van Niekerk <sparky@imaginet.co.za>
2022-03-08 16:51:25 +0200

    BF and DP working

--- knapsack.c
+++ knapsack.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 
+#define MAX_WEIGHT 10
+
 typedef struct item
 {
     char description[25];
@@ -85,23 +87,106 @@ unsigned long bruteforce(item_t *items, int n, int max)
     return combo;
 }
 
+unsigned long which(item_t *items, int **bag, int n, int max)
+{
+    unsigned long combo = 0;
+    int i = n - 1, j = max;
+    while (i > 0)
+    {
+        if (bag[i][j] > bag[i - 1][j]) {
+            combo = (combo | 1) << 1;
+            j = j - items[i].cost;
+        } else {
+            combo = (combo | 0) << 1;
+        }
+        i--;        
+    }
+
+    /* Finally, the first item. */
+    if (bag[i][j] > 0) combo = (combo | 1);
+    return combo;
+}
+
+unsigned long dp(item_t *items, int n, int max)
+{
+    /* Allocate memory for the array. */
+    int i, j, take, notake; 
+    int **s = (int **)malloc((n * sizeof(int*)));
+    unsigned long combo;
+
+    for (i = 0; i < n; i++)
+        s[i] = (int *)malloc((max + 1) * sizeof(int));
+
+    /* Fill in item 1 (we start taking it when the bag is big enough) */
+    for (j = 0; j < items[0].cost; j++) s[0][j] = 0;
+    for (j = items[0].cost; j <= max; j++) s[0][j] = items[0].value;
+
+    /* Fill in the reset of the items */
+    for (i = 1; i < n; i++)
+    {
+        /* Quick fill scenario where bag is too small for items i 
+           (just copy top down) */
+        for (j = 0; j < items[i].cost; j++) s[i][j] = s[i - 1][j];
+
+        /* From here on, we need to make a choice, either take it or leave it. */
+        for (j = items[i].cost; j <= max; j++)
+        {
+            /* Take the item and include all the items of an optimal bag which 
+               is "lighter" from the items 0 - (i-1), i.e. the previous row */               
+            take = items[i].value + s[i - 1][j - items[i].cost];
+
+            /* If we don't take item i, we just have the same items then
+               in previous row, so copy their value. */
+            notake = s[i - 1][j];
+
+            /* Now we select the best between take and notake to decide what 
+               to do (giving preference to taking in case of a tie, but it 
+               does not matter really). */
+            s[i][j] = (take >= notake) ? take : notake;
+        }
+    }
+
+    /* Extract combination of items */
+    combo = which(items, s, n, max);
+
+    printf("best using dp=%i\n", s[n-1][max]);
+
+    /* Free used memory */ 
+    for (i = 0; i < n; i++)
+        free(s[i]);
+    free(s);
+
+    return combo;
+}
+
+
 int main(int argc, char *argv[])
 {
     unsigned long combo;
-    int cnt;
+    int cnt, i, tot;
     item_t *d = NULL;
         
     cnt = get_items("data-small", &d);
     
-    /*printf("found %d\n size=%lu\n", cnt, sizeof(unsigned long));
+    /*printf("found %d\n size=%lu\n", cnt, sizeof(unsigned long));*/
+    tot = 0;
     for (i = 0; i < cnt; i++)
     {
         printf("%s %d %d\n", d[i].description, d[i].value, d[i].cost);
-    }*/
+        tot += d[i].value;
+    }
 
-    combo = bruteforce(d, cnt, 5);
+    printf("Total value of all items: %d\n====================\n", tot);
+
+    combo = bruteforce(d, cnt, MAX_WEIGHT);
+    quickprint(d, cnt, combo);
+
+    printf("====================\n");
+
+    combo = dp(d, cnt, MAX_WEIGHT);
     quickprint(d, cnt, combo);
 
     free(d);
     return 0;
 }
+
